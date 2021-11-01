62b677eef5738161f7814ab81d8e05e8a103839e
olegyev <o.yevstratyev@gmail.com>
2021-11-01 23:49:57 +0300

    Add first service and component

---
+++ src/components/ChallengeComponent.js
@@ -0,0 +1,108 @@
+import * as React from "react";
+import ApiClient from "../services/ApiClient";
+
+class ChallengeComponent extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            a: '',
+            b: '',
+            user: '',
+            message: '',
+            guess: 0
+        };
+        this.handleSubmitResult = this.handleSubmitResult.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    componentDidMount(): void {
+        ApiClient.challenge().then(
+            res => {
+                if (res.ok) {
+                    res.json().then(json => {
+                        this.setState({
+                            a: json.factorA,
+                            b: json.factorB
+                        });
+                    });
+                } else {
+                    this.updateMessage("Can't reach the server");
+                }
+            }
+        );
+    }
+
+    handleChange(event) {
+        const name = event.target.name;
+        this.state({
+            [name]: event.target.value
+        });
+    }
+
+    handleSubmitResult(event) {
+        event.preventDefault();
+        ApiClient.sendGuess(
+            this.state.user,
+            this.state.a,
+            this.state.b,
+            this.state.guess
+        ).then(res => {
+            if (res.ok) {
+                res.json().then(json => {
+                    if (json.correct) {
+                        this.updateMessage("Congrats! Your guess is correct!");
+                    } else {
+                        this.updateMessage("Ouch... Your guess " + json.resultAttempt + " is wrong :(");
+                    }
+                })
+            } else {
+                this.updateMessage("Server error occurred. Try again later.");
+            }
+        });
+    }
+
+    updateMessage(m: string) {
+        this.setState({
+            message: m
+        });
+    }
+
+    render() {
+        return(
+            <div>
+                <div>
+                    <h3>Your new challenge is</h3>
+                    <h1>{this.state.a} X {this.state.b}</h1>
+                </div>
+                <form onSubmit={this.handleSubmitResult}>
+                    <label>
+                        Your alias:
+                        <input 
+                            type="text"
+                            maxLength="12"
+                            name="user"
+                            value={this.state.user}
+                            onChange={this.handleChange}/>
+                    </label>
+                    <br/>
+                    <label>
+                        Your guess:
+                        <input
+                            type="number"
+                            min="0"
+                            name="guess"
+                            value={this.state.guess}
+                            onChange={this.handleChange}/>
+                    </label>
+                    <br/>
+                    <input type="submit" value="Submit"/>
+                </form>
+                <h4>{this.state.message}</h4>
+            </div>
+        );
+    }
+
+}
+
+export default ChallengeComponent;
