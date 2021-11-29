540a0aecc6a328f382081693362537b32166886c
olegyev <o.yevstratyev@gmail.com>
2021-11-30 00:02:56 +0300

    Add LastAttemptsComponent

---
+++ src/components/LastAttemptsComponent.js
@@ -0,0 +1,34 @@
+import * as React from 'react';
+
+class LastAttemptsComponent extends React.Component {
+
+    render() {
+        return(
+            <table>
+                <thead>
+                    <tr>
+                        <th>Challenge</th>
+                        <th>Your guess</th>
+                        <th>Correct</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {this.props.lastAttempts.map(attempt =>
+                        <tr key={attempt.id}
+                            style={{ color: attempt.correct ? 'green' : 'red' }}>
+                            <td>{attempt.factorA} X {attempt.factorB}</td>
+                            <td>{attempt.resultAttempt}</td>
+                            <td>{attempt.correct ? 
+                                "Correct" :
+                                ("Incorrect (" + attempt.factorA * attempt.factorB + ")")}
+                            </td>
+                        </tr>
+                    )}
+                </tbody>
+            </table>
+        );
+    }
+
+}
+
+export default LastAttemptsComponent;
